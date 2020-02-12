683fbe6e791d7a3ebd29448199157590ca110b1c
Zhao Wang <wangz2266@gmail.com>
2020-02-12 14:36:49 -0800

    add inputSetup to input.js module

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const { connect } = require("./client");
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const handleUserInput = data => {
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
-const stdin = setupInput();
-stdin.on("data", handleUserInput);
+const { setupInput } = require("./input");
 
 console.log("Connecting...");
 connect();
+setupInput();
