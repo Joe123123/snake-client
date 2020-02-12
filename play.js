b9c486b7a3018e38f9b003f63bc1601144f48ae5
Zhao Wang <wangz2266@gmail.com>
2020-02-12 13:32:54 -0800

    add play.js

---
+++ play.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "192.168.88.177",
+    port: 50541
+  });
+  conn.setEncoding("utf8");
+  conn.on("data", data => {
+    console.log(data);
+  });
+  return conn;
+};
+console.log("Connecting...");
+connect();
