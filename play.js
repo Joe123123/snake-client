9d89d9edd6673cfa0ada5273333ee66a8e75d897
Zhao Wang <wangz2266@gmail.com>
2020-02-12 13:39:30 -0800

    move connect function to client.js module

--- play.js
+++ play.js
@@ -1,15 +1,4 @@
-const net = require("net");
+const { connect } = require("./client");
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: "192.168.88.177",
-    port: 50541
-  });
-  conn.setEncoding("utf8");
-  conn.on("data", data => {
-    console.log(data);
-  });
-  return conn;
-};
 console.log("Connecting...");
 connect();
