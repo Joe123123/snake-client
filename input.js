e7f40952061c77531ffcb01a60dbd5f8692a4d93
Zhao Wang <wangz2266@gmail.com>
2020-02-12 17:11:39 -0800

    continuous moving and no same direction delay

--- input.js
+++ input.js
@@ -7,22 +7,38 @@ const setupInput = function(conn) {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
+  let myInterval, preKey;
+  let directions = {
+    W: "vertical",
+    S: "vertical",
+    A: "horizontal",
+    D: "horizontal"
+  };
+  const move = key => {
+    if (directions[preKey] !== directions[key]) {
+      clearInterval(myInterval);
+      myInterval = setInterval(() => connection.write(MOVE[key]), 50);
+      preKey = key;
+    }
+  };
   const handleUserInput = data => {
     if (data === "\u0003") {
       process.exit();
     }
+
     switch (data) {
       case "w":
-        connection.write(MOVE.W);
+        move("W");
         break;
       case "s":
-        connection.write(MOVE.S);
+        move("S");
         break;
       case "a":
-        connection.write(MOVE.A);
+        move("A");
         break;
       case "d":
-        connection.write(MOVE.D);
+        move("D");
         break;
       case "q":
         connection.write(SAY.Q);
